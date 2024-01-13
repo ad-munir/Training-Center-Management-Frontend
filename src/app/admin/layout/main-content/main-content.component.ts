95c3012b5703bc555585d07813b74d0e0cb4dd77
ad-munir <mounir.ayad@etu.uae.ac.ma>
2024-01-13 14:24:47 +0100

    Creating layout for admin dashboard

---
+++ src/app/admin/layout/main-content/main-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.css', './../layout.component.css']
+})
+export class MainContentComponent {
+
+}
