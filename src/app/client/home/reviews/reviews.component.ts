28098a93cba31aa40c2c0d809758fd60c099670b
ad-munir <mounir.ayad@etu.uae.ac.ma>
2024-02-01 16:52:19 +0100

    adding reviews section

--- src/app/client/home/reviews/reviews.component.ts
+++ src/app/client/home/reviews/reviews.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Feedback } from './../../../models/feedback.model';
+import { FeedbackService } from 'src/app/services/feedback.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: [ './../home.component.css', './reviews.component.css']
 })
-export class ReviewsComponent {
+export class ReviewsComponent implements OnInit{
 
+  feedbacks!: Feedback[];
+  ngOnInit(): void {
+    this.getFeedbacks()
+  }
+  constructor(private feedbackService: FeedbackService){}
+
+  getFeedbacks() {
+    this.feedbackService.getFeedbacks().subscribe(
+      (data) => {
+        console.log('fetch feedbacks:', data);
+
+        this.feedbacks = data.slice(0, 3);
+      },
+      error => {
+        console.error('Error fetching feedbacks:', error);
+      }
+    );
+  }
 }
